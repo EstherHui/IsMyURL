6505022c352dc9d297bbe4401e542c1ee2bcec9b
EstherHui <92454533+EstherHui@users.noreply.github.com>
2022-03-11 16:39:10 +0800

    Update credentialRegister.js

--- test/entity/credentialRegister.js
+++ test/entity/credentialRegister.js
@@ -14,4 +14,98 @@ export default class Credential {
         this._password = value;
     }
 
+    get invalidemail(){
+        return this._invalidemail;
+    }
+
+    set invalidemail(value){
+        this._invalidemail = value;
+    }
+
+    get existingemail(){
+        return this._existingemail;
+    }
+
+    set existingemail(value){
+        this._existingemail = value;
+    }
+
+    get bpassword(){
+        return this._bpassword;
+    }
+
+    set bpassword(value){
+        this._bpassword = value;
+    }
+
+    get cpassword(){
+        return this._cpassword;
+    }
+    set cpassword(value){
+        this._cpassword = value;
+    }
+
+    get dpassword(){
+        return this._dpassword;
+    }
+    set dpassword(value){
+        this._dpassword = value;
+    }
+
+    get epassword(){
+        return this._epassword;
+    }
+    set epassword(value){
+        this._epassword = value;
+    }
+
+    get ConfirmPassword2(){
+        return this._ConfirmPassword2;
+    }
+    set ConfirmPassword2(value){
+        this._ConfirmPassword2 = value;
+    }
+
+    get verifycodeA(){
+        return this._verifycodeA;
+    }
+    set verifycodeA(value){
+        this._verifycodeA = value;
+    }
+
+    get verifycodeB(){
+        return this._verifycodeB;
+    }
+    set verifycodeB(value){
+        this._verifycodeB = value;
+    }
+
+    get verifycodeC(){
+        return this._verifycodeC;
+    }
+    set verifycodeC(value){
+        this._verifycodeC = value;
+    }
+
+    get verifycodeD(){
+        return this._verifycodeD;
+    }
+    set verifycodeD(value){
+        this._verifycodeD = value;
+    }
+
+    get verifycodeE(){
+        return this._verifycodeE;
+    }
+    set verifycodeE(value){
+        this._verifycodeE = value;
+    }
+
+    get verifycodeF(){
+        return this._verifycodeF;
+    }
+    set verifycodeF(value){
+        this._verifycodeF = value;
+    }
+
 }
