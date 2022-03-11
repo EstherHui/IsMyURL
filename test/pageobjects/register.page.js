a18d566c108974a137f976c1d033c7241c0d7de4
EstherHui <92454533+EstherHui@users.noreply.github.com>
2022-03-11 16:39:39 +0800

    Update register.page.js

--- test/pageobjects/register.page.js
+++ test/pageobjects/register.page.js
@@ -1,16 +1,22 @@
 import Page from './page';
 
-const EMAIL_TXB = '#root > ion-app > div > ion-content > div > div.RegisterPage_form_section__3Kbi_ > form > ion-list > div:nth-child(1) > ion-item > ion-input > input';
-const PASSWORD_TXB = '#root > ion-app > div > ion-content > div > div.RegisterPage_form_section__3Kbi_ > form > ion-list > div:nth-child(2) > ion-item > ion-input > input';
-const CONFIRMPASS_TXB = '#root > ion-app > div > ion-content > div > div.RegisterPage_form_section__3Kbi_ > form > ion-list > div:nth-child(3) > ion-item > ion-input > input';
+const EMAIL_TXB = 'input[name="email"]';
+const PASSWORD_TXB = 'input[name="password"]';
+const CONFIRMPASS_TXB = 'input[name = "confirm_password"]';
 const SIGNUP_BTN = '#root > ion-app > div > ion-content > div > div.RegisterPage_form_section__3Kbi_ > form > ion-button';
 //const THIRDPARTY_BTN = '';
+const verifycodeA = 'input[aria-label="Please enter verification code. Character 1"]';
+const verifycodeB = 'input[aria-label="Character 2"]';
+const verifycodeC = 'input[aria-label="Character 3"]';
+const verifycodeD = 'input[aria-label="Character 4"]';
+const verifycodeE = 'input[aria-label="Character 5"]';
+const verifycodeF = 'input[aria-label="Character 6"]';
 
 class RegisterPage extends Page {
 
     open() {
 
-        return super.open('register/');
+        return super.open('register');
 
     }
 
@@ -25,5 +31,17 @@ class RegisterPage extends Page {
 
     }
 
+    async verifycode(A,B,C,D,E,F){
+
+        await(await $(verifycodeA)).setValue(A);
+        await(await $(verifycodeB)).setValue(B);
+        await(await $(verifycodeC)).setValue(C);
+        await(await $(verifycodeD)).setValue(D);
+        await(await $(verifycodeE)).setValue(E);
+        await(await $(verifycodeF)).setValue(F);
+
+
+    }
+
 }
-export default new RegisterPage();
+export default new RegisterPage;
