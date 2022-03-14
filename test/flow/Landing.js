d9ff138bcdb4622589143647c7599841ad0dcfd3
LAPTOP-B0DFG8D0\Pzhui <pangzhihui1220@gmail.com>
2022-03-14 11:55:19 +0800

    shorten URL generate - done

--- test/flow/Landing.js
+++ test/flow/Landing.js
@@ -6,11 +6,47 @@ export default class Landingg{
         this._testdata = testdata;
     }
 
+    async verifyErrorMessage(content){
+
+        let message = $('[class="error-message-box"]');
+        await expect(message).toHaveTextContaining(content);
+
+    }
+
+    async verifyInvalid(content){
+
+        let error = $('.hydrated.ion-color.ion-color-danger.ios');
+        await expect(error).toHaveTextContaining(content);
+
+    }
+
+    async verifyShortenGenerate(content){
+
+        let success = $('.hydrated.ion-color.ion-color-success.ios');
+        await expect(success).toHaveTextContaining(content);
+
+    }
+
     async ShortenBlank(){
 
         await Landing.open();
         await Landing.ShortenURLCredential(null);
 
     }
+
+    async ShortenInvalid(){
+
+        await Landing.open();
+        await Landing.ShortenURLCredential(this._testdata.invalidlink);
+
+    }
+
+    async ShortenLink(){
+
+        await Landing.open();
+        await Landing.ShortenURLCredential(this._testdata.link);
+
+    }
+
 
 }
--- test/specs/landing.js
+++ test/specs/landing.js
@@ -9,7 +9,24 @@ describe ('Landing001 - ',() => {
     it ('T002 - shorten blank', async() => {
         
      await landingflow.ShortenBlank();
+     await landingflow.verifyErrorMessage('Url Is Required')
 
     });
+
+    it ('T003 - shorten invalid', async() => {
+        
+         await landingflow.ShortenInvalid();
+         await landingflow.verifyInvalid('Invalid Url');
+    
+        });
+
+    it('T004 - generate shorten link', async() => {
+
+        await landingflow.ShortenLink();
+        await landingflow.verifyShortenGenerate('Generate Successfully');
+
+    });
+
+
 
 });
